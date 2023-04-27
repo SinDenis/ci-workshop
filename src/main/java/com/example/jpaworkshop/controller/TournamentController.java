a2a982f258c170926cb939163e2f8447750ec858
d.sin <d.sin@tinkoff.ru>
2023-04-27 19:35:25 +0300

    Add new rest method GET /api/tournament/hello

--- src/main/java/com/example/jpaworkshop/controller/TournamentController.java
+++ src/main/java/com/example/jpaworkshop/controller/TournamentController.java
@@ -1,9 +1,16 @@
 package com.example.jpaworkshop.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/tournament")
 public class TournamentController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
 }
